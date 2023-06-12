4a51adf4860f5905981184dc3f3135326187d1f0
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-06-12 23:12:09 +0200

    Data for Reviews made

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'ana petrovic',
+      job: 'artist',
+      image:
+        'https://i.pinimg.com/564x/ec/63/81/ec6381954a69aa5b1a4cfee33df94b05.jpg',
+      text:
+        "My trip to Paris was absolutely amazing! The city's charm and beauty exceeded all my expectations. From strolling along the Seine River to exploring the iconic landmarks like the Eiffel Tower and Louvre Museum, every moment was filled with wonder and delight. I can't wait to visit Paris again and create more incredible memories.",
+    },
+    {
+      id: 2,
+      name: 'nevena maksic',
+      job: 'photographer',
+      image:
+        'https://i.pinimg.com/564x/36/83/bd/3683bdad4db9c6bae7741ab8ed26593a.jpg',
+      text:
+        "My journey through Ireland was a truly magical experience! From the stunning landscapes of the Cliffs of Moher to the lively streets of Dublin, the country captivated me at every turn. The warm hospitality of the Irish people, combined with the rich history and vibrant culture, made it a trip I will cherish forever.",
+    },
+    {
+      id: 3,
+      name: 'sebastijan vlatkovic',
+      job: 'model',
+      image:
+        'https://i.pinimg.com/564x/e2/79/22/e27922f1798c31424c8074e50535308c.jpg',
+      text:
+        "Exploring Salzburg and Vienna was an absolute delight! Salzburg's baroque architecture and Mozart's birthplace took me back in time, while Vienna's grand palaces and charming coffee houses added a touch of elegance to my trip. The rich history, cultural treasures, and culinary delights of both cities left me in awe and craving for more.",
+    },
+    {
+      id: 4,
+      name: 'jovan kovacec',
+      job: 'actor',
+      image:
+        'https://i.pinimg.com/564x/33/67/80/3367800a6d6c65ec8cb358b807d023d5.jpg',
+      text:
+        "My trip to Rome was a dream come true! The ancient ruins like the Colosseum and Roman Forum left me in awe of the city's rich history, while the mouthwatering cuisine and charming piazzas filled my days with culinary delights and vibrant atmosphere. Rome truly captured my heart and I can't wait to return and explore more of its wonders.",
+    },
+  ];
+  
+  export default reviews;
